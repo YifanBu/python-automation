f059dbd5ead299e4c324931d5853db080bb87096
Yifan Bu <ivanbuau@gmail.com>
2022-10-01 12:36:57 +1000

    adding new file

---
+++ http_request.py
@@ -0,0 +1,11 @@
+# python -m pip install requests
+import requests
+url = "https://icanhazdadjoke.com/search"
+response = requests.get(
+  url, 
+  headers={"Accept": "application/json"},
+  params={"term": "cat", "limit": 1}
+)
+
+data = response.json()
+print(data["results"])
